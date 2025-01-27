598c575adbee7852457afe6170a8b0a09174e63c
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-27 20:28:06 +0530

    wip

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
-export type show<t> = { [k in keyof t]: t[k] } & unknown;
+export type show<t> = {
+	[k in keyof t]: t[k];
+} & unknown;
+
+type Token =
+	| { type: "number"; value: number }
+	| { type: "symbol"; value: string }
+	| { type: "string"; value: string }
+	| { type: "boolean"; value: boolean };
 
 type State<
 	unscanned extends string,
-	current extends unknown[] = unknown[],
-	stack extends unknown[][] = unknown[][],
+	current extends Token[] = Token[],
+	stack extends Token[][] = Token[][],
 > = {
 	unscanned: unscanned;
 	current: current;
@@ -21,53 +29,100 @@ type shiftNumber<s extends State<any>> =
 	s["unscanned"] extends `${infer digit extends number}${infer rest}`
 		? State<
 				trimLeft<rest>,
-				[...s["current"], digit],
+				[...s["current"], { type: "number"; value: digit }],
 				s["stack"]
 			>
 		: s;
 
-type shiftChar<
+type shiftBoolean<s extends State<any>> =
+	s["unscanned"] extends `true${infer rest}`
+		? State<
+				trimLeft<rest>,
+				[...s["current"], { type: "boolean"; value: true }],
+				s["stack"]
+			>
+		: s["unscanned"] extends `false${infer rest}`
+			? State<
+					trimLeft<rest>,
+					[
+						...s["current"],
+						{ type: "boolean"; value: false },
+					],
+					s["stack"]
+				>
+			: s;
+
+type isDelimiter<char extends string> = char extends
+	| " "
+	| "("
+	| ")"
+	| '"'
+	? true
+	: false;
+
+type accumulateSymbol<
+	str extends string,
+	acc extends string = "",
+> = str extends `${infer char}${infer rest}`
+	? isDelimiter<char> extends true
+		? [acc, str]
+		: accumulateSymbol<rest, `${acc}${char}`>
+	: [acc, str];
+
+type shiftSymbol<s extends State<any>> =
+	s["unscanned"] extends `${infer char}${infer rest}`
+		? isDelimiter<char> extends true
+			? s
+			: accumulateSymbol<s["unscanned"]> extends [
+						infer sym extends string,
+						infer remaining extends string,
+					]
+				? State<
+						trimLeft<remaining>,
+						[
+							...s["current"],
+							{ type: "symbol"; value: sym },
+						],
+						s["stack"]
+					>
+				: s
+		: s;
+
+type shiftString<s extends State<any>> =
+	s["unscanned"] extends `"${infer content}"${infer rest}`
+		? State<
+				trimLeft<rest>,
+				[
+					...s["current"],
+					{ type: "string"; value: content },
+				],
+				s["stack"]
+			>
+		: s;
+
+type consumeChar<
 	s extends State<any>,
-	ch extends string,
+	ch extends string | number,
 > = trimLeft<
 	s["unscanned"] extends `${ch}${infer rest}`
 		? rest
 		: s["unscanned"]
 >;
 
-type reduce<s extends State<any>> = State<
-	s["unscanned"],
-	s["current"],
-	s["stack"]
->;
-
-type parseSymbol<s extends State<any>> =
-	s["unscanned"] extends `${infer char}${infer rest}`
-		? char extends " " | "(" | ")"
-			? s
-			: State<
-					trimLeft<rest>,
-					[...s["current"], char],
-					s["stack"]
-				> & {
-					symbol: char;
-				}
-		: s;
-
 type pushStack<s extends State<any>> = State<
-	trimLeft<shiftChar<s, "(">>,
+	consumeChar<s, "(">,
 	[],
 	[...s["stack"], s["current"]]
 >;
 
 type popStack<s extends State<any>> = s["stack"] extends [
-	...infer prevStack extends unknown[][],
-	infer lastItem extends unknown[],
+	...infer stack extends Token[][],
+	infer tail extends Token[],
 ]
 	? State<
-			shiftChar<s, ")">,
-			[...lastItem, s["current"]],
-			prevStack
+			consumeChar<s, ")" | " ">,
+			[...tail, ...s["current"]],
+			stack
 		>
 	: never;
 
@@ -75,10 +130,22 @@ type parse<s extends State<any>> = s["unscanned"] extends ""
 	? s["current"]
 	: s["unscanned"] extends `(${string}`
 		? parse<pushStack<s>>
-		: s["unscanned"] extends `)${string}`
+		: s["unscanned"] extends `${")" | " "}${string}`
 			? parse<popStack<s>>
-			: s["unscanned"] extends `${number}${string}`
-				? parse<shiftNumber<s>>
-				: parse<parseSymbol<s>>;
+			: s["unscanned"] extends `"${string}`
+				? parse<shiftString<s>>
+				: s["unscanned"] extends `${number}${string}`
+					? parse<shiftNumber<s>>
+					: s["unscanned"] extends `${"true" | "false"}${string}`
+						? parse<shiftBoolean<s>>
+						: parse<shiftSymbol<s>>;
 
-type result1 = parse<initialState<`(add (add 12 2) 3)`>>;
+type result1 = parse<initialState<"(add 2 3)">>;
+type result2 = parse<
+	initialState<'(concat "hello" "world")'>
+>;
+type result3 = parse<initialState<"(if true 1 0)">>;
+
+type test1 = show<result1>;
+type test2 = show<result2>;
+type test3 = show<result3>;
