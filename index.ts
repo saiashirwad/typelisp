7169926f738cff9ba15ea974bbf04103d00463d8
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-27 18:58:20 +0530

    wip

--- index.ts
+++ index.ts
@@ -1,44 +1,32 @@
 export type show<t> = { [k in keyof t]: t[k] } & unknown;
 
-type BranchState = {
-	current: unknown[];
-	groups: unknown[][];
-};
-
 type State<
 	unscanned extends string,
-	branches extends BranchState = BranchState,
+	current extends unknown[] = unknown[],
+	groups extends unknown[][] = unknown[][],
 > = {
 	unscanned: unscanned;
-	branches: branches;
+	current: current;
+	groups: groups;
 };
 
-type emptyBranches = {
-	current: [];
-	groups: [];
-};
-
-type initialState<T extends string> = State<
-	T,
-	emptyBranches
->;
+type initialState<T extends string> = State<T, [], []>;
 
 type trimLeft<s extends string> =
 	s extends `${" " | "\n" | "\t"}${infer rest}`
 		? trimLeft<rest>
 		: s;
 
-type appendCurrent<s extends State<any>, value> = show<{
-	groups: s["branches"]["groups"];
-	current: [...s["branches"]["current"], value];
-}>;
-
-type parseNumber<s extends State<any>> =
+type pushNumber<s extends State<any>> =
 	s["unscanned"] extends `${infer digit extends number}${infer rest}`
-		? State<trimLeft<rest>, appendCurrent<s, digit>>
+		? State<
+				trimLeft<rest>,
+				[...s["current"], digit],
+				s["groups"]
+			>
 		: s;
 
-type scanChar<
+type pushChar<
 	s extends State<any>,
 	ch extends string,
 > = trimLeft<
@@ -51,44 +39,40 @@ type parseSymbol<s extends State<any>> =
 	s["unscanned"] extends `${infer char}${infer rest}`
 		? char extends " " | "(" | ")"
 			? s
-			: State<trimLeft<rest>, appendCurrent<s, char>> & {
+			: State<
+					trimLeft<rest>,
+					[...s["current"], char],
+					s["groups"]
+				> & {
 					symbol: char;
 				}
 		: s;
 
 type pushGroup<s extends State<any>> = State<
-	trimLeft<scanChar<s, "(">>,
-	{
-		current: [];
-		groups: [
-			...s["branches"]["groups"],
-			s["branches"]["current"],
-		];
-	}
+	trimLeft<pushChar<s, "(">>,
+	[],
+	[...s["groups"], s["current"]]
 >;
 
-type popGroup<s extends State<any>> =
-	s["branches"]["groups"] extends [
-		...infer prevGroups extends unknown[][],
-		infer lastGroup extends unknown[],
-	]
-		? State<
-				scanChar<s, ")">,
-				{
-					current: [...lastGroup, s["branches"]["current"]];
-					groups: prevGroups;
-				}
-			>
-		: never;
+type popGroup<s extends State<any>> = s["groups"] extends [
+	...infer prevGroups extends unknown[][],
+	infer lastGroup extends unknown[],
+]
+	? State<
+			pushChar<s, ")">,
+			[...lastGroup, s["current"]],
+			prevGroups
+		>
+	: never;
 
 type parse<s extends State<any>> = s["unscanned"] extends ""
-	? s["branches"]["current"]
+	? s["current"]
 	: s["unscanned"] extends `(${string}`
 		? parse<pushGroup<s>>
 		: s["unscanned"] extends `)${string}`
 			? parse<popGroup<s>>
 			: s["unscanned"] extends `${number}${string}`
-				? parse<parseNumber<s>>
+				? parse<pushNumber<s>>
 				: parse<parseSymbol<s>>;
 
-type result1 = parse<initialState<"(add (add 1 2) 3)">>;
+type result1 = parse<initialState<`(add (add 12 2) 3)`>>;
